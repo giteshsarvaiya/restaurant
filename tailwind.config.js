f22caa14e41fcff67f6747f700c3796809ef5342
Gitesh <gitesh.sarvaiya28@gmail.com>
2023-08-07 23:18:37 +0530

    project initialise

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'royal-brown': '#530004',
+        'royal-pale': '#C7A350'
+      },
+    },
+  },
+  plugins: [],
+}
+
